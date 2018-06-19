622cff537365ed6b7aa120cebf77ed7f38943495
Wusung Peng <twkoci@gmail.com>
2018-06-20 06:39:26 +0800

    core block service

--- core/core.go
+++ core/core.go
@@ -1,10 +1,13 @@
 package core
 
 import (
-	"../config"
-	ds "../datastore"
 	"fmt"
+	ds "../datastore"
+	blocks "../blocks"
+	"../config"
+	merkledag "../merkledag"
 	"../peer"
+	u "../util"
 )
 
 // IPFS Core module. It represents an IPFS instance.
@@ -33,7 +36,7 @@ type IpfsNode struct {
 	// BitSwap *bitswap.BitSwap
 
 	// the block service, get/add blocks.
-	// Blocks *blocks.BlockService
+	Blocks *blocks.BlockService
 
 	// the path resolution system
 	// Resolver *resolver.PathResolver
@@ -52,11 +55,31 @@ func NewIpfsNode(cfg *config.Config) (*IpfsNode, error) {
 		return nil, err
 	}
 
+	bs, err := blocks.NewBlockService(d)
+	if err != nil {
+		return nil, err
+	}
+
 	n := &IpfsNode{
 		Config:    cfg,
 		PeerBook:  &peer.PeerBook{},
 		Datastore: d,
+		Blocks:    bs,
 	}
 
 	return n, nil
+}
+
+func (n *IpfsNode) AddDagNode(nd *merkledag.Node) (u.Key, error) {
+	d, err := nd.Encoded(false)
+	if err != nil {
+		return "", err
+	}
+
+	b, err := blocks.NewBlock(d)
+	if err != nil {
+		return "", err
+	}
+
+	return n.Blocks.AddBlock(b)
 }
