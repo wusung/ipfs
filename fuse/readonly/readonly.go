26b05b8d99c38e94a924dfbadc0b3e961108ae44
Wusung Peng <twkoci@gmail.com>
2018-06-20 09:15:40 +0800

    fuse works now

--- fuse/readonly/readonly.go
+++ fuse/readonly/readonly.go
@@ -3,11 +3,17 @@
 package readonly
 
 import (
-	"os"
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
-	"../../core"
-	mdag "../../merkledag"
+	"fmt"
+	core "github.com/jbenet/go-ipfs/core"
+	mdag "github.com/jbenet/go-ipfs/merkledag"
+	"os"
+	"os/exec"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
 )
 
 type FileSystem struct {
@@ -19,18 +25,24 @@ func NewFileSystem(ipfs *core.IpfsNode) *FileSystem {
 }
 
 func (f FileSystem) Root() (fs.Node, fuse.Error) {
-	return Root{Ipfs: f.Ipfs}, nil
+	return &Root{Ipfs: f.Ipfs}, nil
 }
 
 type Root struct {
 	Ipfs *core.IpfsNode
 }
 
-func (Root) Attr() fuse.Attr {
-	return fuse.Attr{Inode: 1, Mode: os.ModeDir | 0111} // -rw+x
+func (*Root) Attr() fuse.Attr {
+	return fuse.Attr{Mode: os.ModeDir | 0111} // -rw+x
 }
 
 func (s *Root) Lookup(name string, intr fs.Intr) (fs.Node, fuse.Error) {
+
+	switch name {
+	case "mach_kernel", ".hidden", "._.":
+		// Just quiet some log noise on OS X.
+		return nil, fuse.ENOENT
+	}
 
 	nd, err := s.Ipfs.Resolver.ResolvePath(name)
 	if err != nil {
@@ -38,10 +50,10 @@ func (s *Root) Lookup(name string, intr fs.Intr) (fs.Node, fuse.Error) {
 		return nil, fuse.ENOENT
 	}
 
-	return Node{Ipfs: s.Ipfs, Nd: nd}, nil
+	return &Node{Ipfs: s.Ipfs, Nd: nd}, nil
 }
 
-func (Root) ReadDir(intr fs.Intr) ([]fuse.Dirent, fuse.Error) {
+func (*Root) ReadDir(intr fs.Intr) ([]fuse.Dirent, fuse.Error) {
 	return nil, fuse.EPERM
 }
 
@@ -50,12 +62,10 @@ type Node struct {
 	Nd   *mdag.Node
 }
 
-func (s Node) Attr() fuse.Attr {
-
+func (s *Node) Attr() fuse.Attr {
 	if len(s.Nd.Links) > 0 {
 		return fuse.Attr{Mode: os.ModeDir | 0555}
 	}
-
 	size, _ := s.Nd.Size()
 	return fuse.Attr{Mode: 0444, Size: uint64(size)}
 }
@@ -68,7 +78,7 @@ func (s *Node) Lookup(name string, intr fs.Intr) (fs.Node, fuse.Error) {
 		return nil, fuse.ENOENT
 	}
 
-	return Node{Ipfs: s.Ipfs, Nd: nd}, nil
+	return &Node{Ipfs: s.Ipfs, Nd: nd}, nil
 }
 
 func (s *Node) ReadDir(intr fs.Intr) ([]fuse.Dirent, fuse.Error) {
@@ -92,7 +102,16 @@ func (s *Node) ReadAll(intr fs.Intr) ([]byte, fuse.Error) {
 	return []byte(s.Nd.Data), nil
 }
 
-func Mount(ipfs *core.IpfsNode, fpath string) (error) {
+func Mount(ipfs *core.IpfsNode, fpath string) error {
+
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGHUP, syscall.SIGINT,
+		syscall.SIGTERM, syscall.SIGQUIT)
+
+	go func() {
+		<-sigc
+		Unmount(fpath)
+	}()
 
 	c, err := fuse.Mount(fpath)
 	if err != nil {
@@ -111,4 +130,36 @@ func Mount(ipfs *core.IpfsNode, fpath string) (error) {
 		return err
 	}
 	return nil
+}
+
+// Unmount attempts to unmount the provided FUSE mount point, forcibly
+// if necessary.
+func Unmount(point string) error {
+	fmt.Printf("Unmounting %s...\n", point)
+
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = exec.Command("diskutil", "umount", "force", point)
+	case "linux":
+		cmd = exec.Command("fusermount", "-u", point)
+	default:
+		return fmt.Errorf("unmount: unimplemented")
+	}
+
+	errc := make(chan error, 1)
+	go func() {
+		if err := exec.Command("umount", point).Run(); err == nil {
+			errc <- err
+		}
+		// retry to unmount with the fallback cmd
+		errc <- cmd.Run()
+	}()
+
+	select {
+	case <-time.After(1 * time.Second):
+		return fmt.Errorf("umount timeout")
+	case err := <-errc:
+		return err
+	}
 }
