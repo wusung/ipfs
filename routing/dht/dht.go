f8ebee68b505e4143043914211694f56e26bb984
Wusung Peng <twkoci@gmail.com>
2018-06-20 23:00:50 +0800

    working on upper level dht implementations, protbuf, etc

--- routing/dht/dht.go
+++ routing/dht/dht.go
@@ -1,10 +1,21 @@
 package dht
 
-// TODO. SEE https://github.com/jbenet/node-ipfs/blob/master/submodules/ipfs-dht/index.js
+import (
+	"../../swarm"
+)
 
+// TODO. SEE https://github.com/jbenet/node-ipfs/blob/master/submodules/ipfs-dht/index.js
 
 // IpfsDHT is an implementation of Kademlia with Coral and S/Kademlia modifications.
 // It is used to implement the base IpfsRouting module.
 type IpfsDHT struct {
 	routes RoutingTable
+
+	network *swarm.Swarm
+}
+
+func (dht *IpfsDHT) handleMessages() {
+	for mes := range dht.network.Chan.Incoming {
+
+	}
 }
